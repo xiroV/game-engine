95b32f7b3a854cdbe54e046a6a94f10018d0c976
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-02-15 14:36:33 +0100

    Create window and allow quit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <map>
+
+enum Action
+{
+    MoveUp
+};
+
+bool handle_input(std::map<int, std::pair<Action, bool>> &map);
 
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    std::map<int, std::pair<Action, bool>> map;
+    auto &mapRef = map;
+    int i = 0;
     while (true) {
-        if (!handle_input()) {
+        if (!handle_input(mapRef))
+        {
             break;
         };
     }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
 
-bool handle_input() {
+bool handle_input(std::map<int, std::pair<Action, bool>> &map) {
     SDL_Event e;
-    switch (SDL_PollEvent(&e)) {
-        case SDL_KEYDOWN:
-        case SDL_KEYUP:
-        case SDL_QUIT:
-            return false;
+    while (SDL_PollEvent(&e)) {
+        switch (e.key.keysym.sym) {
+            case SDLK_ESCAPE: 
+                if (SDL_KEYDOWN == e.type)
+                    return false;
+                break;
+        }
     }
-    return false;
+    return true;
 }
