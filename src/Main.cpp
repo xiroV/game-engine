c5008f8fbac6580edfb1df24b055c03be6ab4592
Brian Alberg <brian@alberg.org>
2020-10-23 16:10:13 +0200

    Fixed make build on linux

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,38 +19,7 @@ int main(int argc, char* argv[]) {
 #else 
 
 int main(int argc, char* argv[]) {
-    if (TTF_Init() < 0) {
-        std::cout << "Failed to initialize TTF" << std::endl;
-    }   
-    
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    
-    KeyMap key_map;
-    MouseMap mouse_map;
-    ControllerMap controller_map;
-    ControllerList list;
-    Controller c = {
-        false,
-        false,
-        {0, 0},
-        {0, 0},
-        controller_map
-    };
-    list.emplace_back(c);
-    Input input(key_map, mouse_map, list);
-    Engine engine(input);
-    engine.input.start_rebind_keyboard_action(Jump, SDLK_UNKNOWN);
-    bool running = true;
-    while (!engine.input.handle_input()) {}
-    TTF_Quit();
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
     return 0;
 }
+
 #endif
